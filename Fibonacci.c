c1948e666744086b5328a33d785803416a7c6a8d
DhivakarG11 <dhivakargopi1101@gmail.com>
2025-01-03 19:03:46 +0530

    Made intentional Errors

--- Fibonacci.c
+++ Fibonacci.c
@@ -18,7 +18,7 @@ int* generateFibonacci(int n) {
 
 void printSeries(int n, int *arr){
     printf("\n\nGenerated Series: ");
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i--){
         printf("%d ", arr[i]);
     }
 }
